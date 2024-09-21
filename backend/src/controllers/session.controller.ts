1db25d75747181e2751bc366c34e6a73a3dfc865
Victor Eleanya <victoreleanya89@gmail.com>
2024-09-21 13:59:19 +0100

    feat(session.controller.ts): added session controller

--- backend/src/controllers/session.controller.ts
+++ backend/src/controllers/session.controller.ts
@@ -1,6 +1,8 @@
-import { OK } from "../constants/http";
+import { z } from "zod";
+import { NOT_FOUND, OK } from "../constants/http";
 import SessionModel from "../models/session.model";
 import catchErrors from "../utils/catchErrors";
+import appAssert from "../utils/appAssert";
 
 export const getSessionsHandler = catchErrors(async (req, res) => {
   const sessions = await SessionModel.find(
@@ -24,4 +26,14 @@ export const getSessionsHandler = catchErrors(async (req, res) => {
       }),
     }))
   );
+});
+
+export const deleteSessionHandler = catchErrors(async (req, res) => {
+  const sessionId = z.string().parse(req.params.id);
+  const deleted = await SessionModel.findOneAndDelete({
+    _id: sessionId,
+    userId: req.userId,
+  });
+  appAssert(deleted, NOT_FOUND, "Session not found");
+  return res.status(OK).json({ message: "Session deleted" });
 });
