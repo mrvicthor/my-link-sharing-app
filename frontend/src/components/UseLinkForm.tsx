c8ebecd782d0b7551aae7425716892caa4efff84
Victor Eleanya <victoreleanya89@gmail.com>
2024-09-28 23:16:45 +0100

    feat(Links.tsx): added Select field

---
+++ frontend/src/components/UseLinkForm.tsx
@@ -0,0 +1,81 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "./ui/input";
+
+const FormSchema = z.object({
+  platform: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .email(),
+  urlLink: z.string({
+    required_error: "Please enter a link.",
+  }),
+});
+
+export function UseLinkForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="platform"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a verified email to display" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="urlLink"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- frontend/src/pages/Links.tsx
+++ frontend/src/pages/Links.tsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import { z } from "zod";
 import PhoneIcon from "@/assets/images/illustration-empty.svg";
 import PhoneMockup from "@/assets/images/illustration-phone-mockup.svg";
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { options } from "@/lib/constants";
+import LinkIcon from "@/components/LinkIcon";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const Link = z.object({
+  platform: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .email(),
+  urlLink: z.string({
+    required_error: "Please enter a link.",
+  }),
+});
+
+const FormSchema = z.object({
+  links: z.array(Link),
+});
+
 const Links = () => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      links: [
+        {
+          platform: "GitHub",
+          urlLink: "https://example.com",
+        },
+      ],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "links",
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
     <section className="container md:px-6">
       <div className="lg:grid lg:grid-wrapper gap-6">
@@ -9,35 +68,100 @@ const Links = () => {
           <img src={PhoneMockup} alt="phone mockup" />
         </div>
         <div className="box-links bg-[#ffffff] pt-6 rounded-md pb-6">
-          <div className="px-6">
-            <h1 className="font-bold text-2xl">Customize your links</h1>
-            <p className="text-[#737373] text-sm mt-4 opacity-80">
-              Add/edit/remove links below and then share all your profiles with
-              the world{" "}
-            </p>
-            <Button
-              variant={"addButton"}
-              size={"lg"}
-              className="font-bold mt-8"
-            >
-              + Add new link
-            </Button>
-            <section className="bg-[#fafafa] flex flex-col justify-center items-center mt-4 rounded-md gap-y-6 py-10 mb-6">
-              <img src={PhoneIcon} alt="phone icon" />
-              <h2 className="text-2xl font-bold">Let's get you started</h2>
-              <p className="text-center text-[#737373] text-sm opacity-80">
-                Use the "Add new link" button to get started. Once you have more
-                than one link, you can reorder and edit them. We're here to help
-                you share your profiles with everyone.
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <h1 className="font-bold text-2xl px-6">Customize your links</h1>
+              <p className="text-[#737373] text-sm mt-4 opacity-80 px-6">
+                Add/edit/remove links below and then share all your profiles
+                with the world{" "}
               </p>
-            </section>
-          </div>
-          <hr />
-          <div className="flex justify-end mt-4 px-6">
-            <Button variant={"saveButton"} className="w-full md:w-auto">
-              Save
-            </Button>
-          </div>
+              <div className="px-6">
+                <Button
+                  variant={"addButton"}
+                  size={"lg"}
+                  className="font-bold"
+                  onClick={() => {
+                    setIsFormOpen(true);
+                    if (isFormOpen) append({ platform: "", urlLink: "" });
+                  }}
+                >
+                  + Add new link
+                </Button>
+              </div>
+              {isFormOpen ? (
+                <section className="min-h-[27.1875rem] px-6">
+                  {fields.map((item, index) => (
+                    <div>
+                      <Button onClick={() => remove(index)}>Remove</Button>
+                      <FormField
+                        key={item.id}
+                        {...form.register(`links.${index}.platform` as const)}
+                        control={form.control}
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Platform</FormLabel>
+
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                            >
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="GitHub" />
+                                </SelectTrigger>
+                              </FormControl>
+
+                              <SelectContent>
+                                {options.map((option) => (
+                                  <>
+                                    <SelectItem
+                                      value={option.title}
+                                      key={option.id}
+                                      className="relative text-[#737373] py-2"
+                                    >
+                                      <div className="absolute ">
+                                        <LinkIcon
+                                          color=""
+                                          pathString={option.icon}
+                                        />{" "}
+                                      </div>
+                                      <div className="ml-8">{option.title}</div>
+                                    </SelectItem>
+                                    <hr />
+                                  </>
+                                ))}
+                              </SelectContent>
+                            </Select>
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  ))}
+                </section>
+              ) : (
+                <section className="px-6">
+                  <div className="bg-[#fafafa] flex flex-col justify-center items-center mt-4 rounded-md gap-y-6 py-10 mb-6 min-h-[24.1875rem]">
+                    <img src={PhoneIcon} alt="phone icon" />
+                    <h2 className="text-2xl font-bold">
+                      Let's get you started
+                    </h2>
+                    <p className="text-center text-[#737373] text-sm opacity-80">
+                      Use the "Add new link" button to get started. Once you
+                      have more than one link, you can reorder and edit them.
+                      We're here to help you share your profiles with everyone.
+                    </p>
+                  </div>
+                </section>
+              )}
+
+              <hr />
+              <div className="flex justify-end mt-4 px-6">
+                <Button variant={"saveButton"} className="w-full md:w-auto">
+                  Save
+                </Button>
+              </div>
+            </form>
+          </Form>
         </div>
       </div>
     </section>
