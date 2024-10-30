8ce8a0ad215dfff9f5517b165425b54770f703ba
Victor Eleanya <victoreleanya89@gmail.com>
2024-10-30 22:05:46 +0100

    fix(CardInfo.tsx): fix styling

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -10,7 +10,7 @@ const Notification = ({ message }: NotificationProps) => {
 
   if (!notification) return null;
   return (
-    <div className="fixed bottom-[5rem] flex justify-center w-full">
+    <div className="fixed bottom-[2rem] flex justify-center w-full z-50">
       <div
         className={`
          bg-[#333333] rounded-md p-4 text-white m-auto flex gap-2  w-[22rem]`}
